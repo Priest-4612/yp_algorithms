1375c6f9c79a59c8d6ff2463cedf92fe723f927c
priest4612 <prie4612@gmail.com>
2021-10-28 02:37:56 +0300

    Обновил код. Тесты проходит. Тупо плагиат.

--- context_13/broken_search.py
+++ context_13/broken_search.py
@@ -1,33 +1,22 @@
-def binary_search(nums, target, left, right):
-    if right <= left:
-        return -1
-    mid = (left + right) // 2
-    if nums[mid] == target:
-        return target
-    elif target < nums[mid]:
-        return binary_search(nums, target, left, mid)
-    else:
-        return binary_search(nums, target, mid + 1, right)
-
-
 def broken_search(nums, target) -> int:
-    array_legth = len(nums)
     left = 0
-    right = array_legth - 1
-    if array_legth == 1:
-        if nums[left] == target:
-            return left
+    right = len(nums) - 1
+    while left <= right:
+        mid = (left + right) // 2
+        if nums[mid] == target:
+            return mid
+        if nums[left] <= nums[mid]:
+            if target < nums[mid] and target >= nums[left]:
+                right = mid - 1
+            else:
+                left = mid + 1
+        else:
+            if target > nums[mid] and target <= nums[right]:
+                left = mid + 1
+            else:
+                right = mid - 1
+    else:
         return -1
-    while nums[left] >= nums[right]:
-        if nums[right] == target:
-            return right
-        right -= 1
-    return binary_search(nums, target, left, right)
-
-
-def test():
-    arr = [19, 21, 100, 101, 1, 4, 5, 7, 12]
-    assert broken_search(arr, 5) == 6
 
 
 if __name__ == '__main__':
