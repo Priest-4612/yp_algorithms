e12dea93370d383a2476c4ab9fc675a48665dc65
priest4612 <prie4612@gmail.com>
2021-10-29 02:35:50 +0300

    Обновил код

--- context_13/quicksort_inplace.py
+++ context_13/quicksort_inplace.py
@@ -1,10 +1,23 @@
-def quicksort_inplace(list, start=0, end=None):
+def to_participant(login, count_problem, amount_fine):
+    return (-int(count_problem), int(amount_fine), login)
+
+
+def subpart_partition(array, start, end, paviot_index):
+    array[start], array[paviot_index] = array[paviot_index], array[start]
+    paviot = array[start]
+
+
+
+
+
+def quicksort_inplace(array, start=0, end=None):
     pass
 
 
 if __name__ == '__main__':
     count_participants = int(input())
-    entries = [input().split(' ') for _ in range(count_participants)]
+    entries = [to_participant(*input().split(' '))
+               for _ in range(count_participants)]
 
     print(
         f'Количество участников: {count_participants}'
