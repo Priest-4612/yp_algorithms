5c9806e28e776871047b15ec9ff0f21a7bd45bb5
priest4612 <prie4612@gmail.com>
2021-10-30 16:29:16 +0300

    Добавил решение задачи

--- context_13/quicksort_inplace.py
+++ context_13/quicksort_inplace.py
@@ -1,25 +1,43 @@
+import random
+
+
 def to_participant(login, count_problem, amount_fine):
     return (-int(count_problem), int(amount_fine), login)
 
 
 def subpart_partition(array, start, end, paviot_index):
+    if not (start <= paviot_index <= end):
+        raise ValueError('paviot_index должна быть между start и end')
     array[start], array[paviot_index] = array[paviot_index], array[start]
     paviot = array[start]
+    partition = start + 1
+    curren = start + 1
+    while curren <= end:
+        if array[curren] <= paviot:
+            array[curren], array[partition] = array[partition], array[curren]
+            partition += 1
+        curren += 1
 
-
-
+    array[start], array[partition - 1] = array[partition - 1], array[start]
+    return partition - 1
 
 
 def quicksort_inplace(array, start=0, end=None):
-    pass
+    if end is None:
+        end = len(array) - 1
+    if end - start < 1:
+        return
+
+    paviot_index = random.randint(start, end)
+    partition = subpart_partition(array, start, end, paviot_index)
+    quicksort_inplace(array, start, partition - 1)
+    quicksort_inplace(array, partition + 1, end)
 
 
 if __name__ == '__main__':
     count_participants = int(input())
-    entries = [to_participant(*input().split(' '))
+    entries = [to_participant(*input().split())
                for _ in range(count_participants)]
-
-    print(
-        f'Количество участников: {count_participants}'
-        f'\nСписки участников {entries}'
-    )
+    quicksort_inplace(entries)
+    result = (login for _, _, login in entries)
+    print(*result, sep='\n')
