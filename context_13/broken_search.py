a438427ed03b70de97c78783e127b06f7e021a8c
Priest4612 <prie4612@gmail.com>
2021-11-01 16:11:17 +0300

    Обновил код

--- context_13/broken_search.py
+++ context_13/broken_search.py
@@ -5,18 +5,18 @@ def broken_search(nums, target) -> int:
     while left <= right:
         mid = (left + right) // 2
         mid_element = nums[mid]
-        left_element = nums[left]
-        right_element = nums[right]
         if mid_element == target:
             return mid
+        left_element = nums[left]
+        if left_element == target:
+            return left
         if left_element < mid_element or left_element == mid_element:
-            if left_element == target:
-                return left
             if left_element < target < mid_element:
                 right = mid - 1
             else:
                 left = mid + 1
         else:
+            right_element = nums[right]
             if target == right_element:
                 return right
             if mid_element < target < right_element:
